7f267290f2cd894257f20f55a1cc0574f9db6bf0
ambhatt2 <ambhatt2@ncsu.edu>
2023-07-27 17:58:00 -0400

    update stuff

--- functionality.py
+++ functionality.py
@@ -2,11 +2,15 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer as strainer
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
+import requests
+import re
 from selenium.webdriver.common.action_chains import ActionChains 
+import spacy
 
 
     
@@ -70,6 +74,33 @@ def scrollToBottom():
         if round(end - start) > 6:
             break
 
+def scrollLittle():
+    start = time.time()
+ 
+    # will be used in the while loop
+    initialScroll = 0
+    finalScroll = 500
+ 
+    while True:
+        driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
+        # this command scrolls the window starting from
+        # the pixel value stored in the initialScroll
+        # variable to the pixel value stored at the
+        # finalScroll variable
+        initialScroll = finalScroll
+        finalScroll += 1000
+ 
+        # we will stop the script for 3 seconds so that
+        # the data can load
+        time.sleep(2)
+        # You can change it as per your needs and internet speed
+ 
+        end = time.time()
+ 
+        # scroll for 4 seconds
+        if round(end - start) > 4:
+            break
+
 def recruitmentInitiate():
     # Filtering by specific user-requested company/companies (add multiple companies functionality later)
     userCompany = input('What company are you interested in?\n')
@@ -84,13 +115,63 @@ def recruitmentInitiate():
     # scrolls to bottom to load all elements of webpage
     scrollToBottom()
 
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[text()='People']"))).click()
-    time.sleep(10)
-    query = driver.find_element(By.ID, "people-search-keywords")
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(query)).click()
-    query.send_keys('recruitment')
-    query.send_keys(Keys.ENTER)
+    curr_url = driver.current_url
+    driver.get(curr_url + "people/?keywords=recruitment")
 
+
+    scrape()
+
+def scrape():
+    #GETTING ALL PROFILE URLS
+    scrollLittle()
+    lnks=driver.find_elements(By.TAG_NAME,"a")
+    # traverse list
+    linkStrings = []
+    for lnk in lnks:
+    # get_attribute() to get all href
+        linkStrings.append(lnk.get_attribute("href"))
+
+    print(len(linkStrings))
+    del linkStrings[0:25]
+    del linkStrings[len(linkStrings) - 83:]
+    
+    linkNews = [link for link in linkStrings if filterLinks(link)]
+    #filterLinks2(linkStrings)
+
+
+def filterLinks(url):
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(cc, 'html.parser')
+
+    # Check if the page contains the LinkedIn profile indicator
+    profile_indicator = soup.find('code', attrs={'class': 'top-card-primary-content__title'})
+    if profile_indicator:
+        return True
+
+    return False
+
+
+""""
+def filterLinks2(urls):
+    # Sample list of HTML links
+
+    # Load the spaCy English model
+    nlp = spacy.load('en_core_web_sm')
+    unique_links = set()
+
+    # Iterate over the HTML links and search for names
+    for link in urls:
+        doc = nlp(link)
+        for token in doc:
+            if token.like_url:
+                unique_links.add(token.text)
+                print(token.text)
+"""
+
+    
+    
+            
     
 
 loginInitiate()
