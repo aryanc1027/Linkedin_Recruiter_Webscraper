b3a02c374057a3cffb453c624afb63cf919060fa
ambhatt2 <ambhatt2@ncsu.edu>
2023-08-06 10:40:13 -0400

    documentation

--- Scroll.py
+++ Scroll.py
@@ -4,27 +4,22 @@ import time
 def scrollToBottom(driver):
     start = time.time()
 
-    # will be used in the while loop
     initialScroll = 0
     finalScroll = 1000
 
     while True:
         driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
-        # this command scrolls the window starting from
-        # the pixel value stored in the initialScroll
-        # variable to the pixel value stored at the
-        # finalScroll variable
+        # Command scrolls from initial pixel 
+        # value to final pixel value
         initialScroll = finalScroll
         finalScroll += 1000
 
-        # we will stop the script for 3 seconds so that
-        # the data can load
+        # Allowing data to load completely
         time.sleep(3)
-        # You can change it as per your needs and internet speed
 
         end = time.time()
 
-        # scroll for 6 seconds
+        # Scrolls for 6 seconds
         if round(end - start) > 6:
             break
 
@@ -32,26 +27,21 @@ def scrollToBottom(driver):
 def scrollLittle(driver):
     start = time.time()
 
-    # will be used in the while loop
     initialScroll = 0
     finalScroll = 500
 
     while True:
         driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
-        # this command scrolls the window starting from
-        # the pixel value stored in the initialScroll
-        # variable to the pixel value stored at the
-        # finalScroll variable
+        # Command scrolls from initial pixel 
+        # value to final pixel value
         initialScroll = finalScroll
         finalScroll += 1000
 
-        # we will stop the script for 3 seconds so that
-        # the data can load
+        # Allowing data to load completely
         time.sleep(2)
-        # You can change it as per your needs and internet speed
 
         end = time.time()
 
-        # scroll for 4 seconds
+        # Scrolls for 4 seconds
         if round(end - start) > 4:
             break
